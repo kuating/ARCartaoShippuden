368480a7fc506b9ba00db856919105089a24f58f
kuating <34279732+kuating@users.noreply.github.com>
2020-01-27 18:08:12 -0300

    Backend-Done Frontend-Minor Updates

---
+++ Assets/Scripts/ScrollerSize.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScrollerSize : MonoBehaviour
+{
+    private int lineCount = 0;
+
+    private void Awake()
+    {
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(100,5*100 - 10);
+    }
+
+    void Update()
+    {
+        if (lineCount != this.transform.childCount)
+        {
+            lineCount = this.transform.childCount;
+            if (this.lineCount > 5)
+            {
+                this.GetComponent<RectTransform>().sizeDelta = new Vector2(100, lineCount * 100 - 10);
+                this.GetComponent<RectTransform>().localPosition = new Vector2(this.GetComponent<RectTransform>().localPosition.x, -100 * (lineCount - 5));
+            }
+            else this.GetComponent<RectTransform>().sizeDelta = new Vector2(100, 5 * 100 - 10);
+        }
+    }
+}
